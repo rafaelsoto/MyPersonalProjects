f1497e4016db91d11341541de9050a9f7b60c1f0
Rafael Almeida Fernandez Soto <01057075540@serpro-1452916.bsa.serpro>
2010-09-09 18:20:17 -0300

    Modificação do mapa de requisitos

--- AWM/AWM.mm
+++ AWM/AWM.mm
@@ -1,6 +1,105 @@
 <map version="0.8.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1283365153325" ID="Freemind_Link_822154664" MODIFIED="1283365157484" TEXT="AWM">
-<node CREATED="1283365169551" ID="_" MODIFIED="1283365172978" POSITION="right" TEXT="Objetivo"/>
+<node CREATED="1283365153325" ID="Freemind_Link_822154664" MODIFIED="1283373153976" TEXT="GeoSpatial SDI">
+<font NAME="SansSerif" SIZE="20"/>
+<node CREATED="1283365559423" ID="_" MODIFIED="1283373149611" POSITION="right" TEXT="Produtos">
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node CREATED="1283372419435" FOLDED="true" ID="Freemind_Link_1355747311" MODIFIED="1283373615254" TEXT="GeoDataCatalog">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1283372423381" ID="Freemind_Link_1851406806" MODIFIED="1283372428578" TEXT="Base de Dados Vetorial"/>
+<node CREATED="1283372428904" ID="Freemind_Link_389628904" MODIFIED="1283372433803" TEXT="Base de Dados Raster"/>
+</node>
+<node CREATED="1283366042542" FOLDED="true" ID="Freemind_Link_1164750044" MODIFIED="1283373621305" TEXT="GeoDataCatalogManagement">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1283366048710" ID="Freemind_Link_866595708" MODIFIED="1283370943850" TEXT="Objetivo: Ferramenta responsavel por interfaciar a inclus&#xe3;o de novos dados geogr&#xe1;ficos junto a base de dados corporativa"/>
+<node CREATED="1283366092599" ID="Freemind_Link_1642644631" MODIFIED="1283366094480" TEXT="Features">
+<node CREATED="1283366104509" ID="Freemind_Link_1950436896" MODIFIED="1283366127039" TEXT="Cadastro dos metadados da informa&#xe7;&#xe3;o geogr&#xe1;fica"/>
+<node CREATED="1283366130515" ID="Freemind_Link_683464563" MODIFIED="1283371066719" TEXT="Importa&#xe7;&#xe3;o de shapefile para a base de dados geogr&#xe1;fico (Vetorial e Matricial)"/>
+<node CREATED="1283366215439" ID="Freemind_Link_454906000" MODIFIED="1283370955680" TEXT="Convers&#xe3;o de sistema de proje&#xe7;&#xe3;o"/>
+<node CREATED="1283371626804" ID="Freemind_Link_362539822" MODIFIED="1283371643723" TEXT="Gerenciamento de multiplas bases de dados espaciais"/>
+</node>
+</node>
+<node CREATED="1283371726306" FOLDED="true" HGAP="22" ID="Freemind_Link_1424178479" MODIFIED="1283374173353" TEXT="GeoserverEnterprise" VSHIFT="-6">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1283371735628" ID="Freemind_Link_1389763716" MODIFIED="1283371771747" TEXT="Objetivo: Ter uma instancia de geoserver configurada para se adaptar a diversos ambientes"/>
+<node CREATED="1283371772630" ID="Freemind_Link_1948148603" MODIFIED="1283371776139" TEXT="Features">
+<node CREATED="1283371777535" ID="Freemind_Link_851361220" MODIFIED="1283371781921" TEXT="Geoserver para Tomcat"/>
+<node CREATED="1283371782260" ID="Freemind_Link_1074771838" MODIFIED="1283371786306" TEXT="Geoserver para Jboss"/>
+<node CREATED="1283371786778" ID="Freemind_Link_1992711580" MODIFIED="1283371794116" TEXT="Geoserver para Jboss Cluster"/>
+<node CREATED="1283371995920" ID="Freemind_Link_1284316457" MODIFIED="1283372014564" TEXT="Configura&#xe7;&#xf5;es de seguran&#xe7;a"/>
+</node>
+</node>
+<node CREATED="1283373073777" FOLDED="true" ID="Freemind_Link_1143255615" MODIFIED="1283373669243" TEXT="GeoMetaCatalogManagement">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1283373081048" ID="Freemind_Link_122158298" MODIFIED="1283373111268" TEXT="Objetivo: Prover uma base de metadados que ser&#xe1; integrada aos produtos GeoWebMappingPortal e GeoDatasourceManagement"/>
+<node CREATED="1283373633867" ID="Freemind_Link_842427359" MODIFIED="1283373637125" TEXT="Features">
+<node CREATED="1283373638027" ID="Freemind_Link_109063274" MODIFIED="1283373642167" TEXT="Baseado no geonetwork"/>
+<node CREATED="1283373642473" ID="Freemind_Link_1358863014" MODIFIED="1283373650317" TEXT="Catologo no padr&#xe3;o ISO90115"/>
+</node>
+</node>
+<node CREATED="1283372052025" FOLDED="true" ID="Freemind_Link_678212356" MODIFIED="1283373165924" TEXT="GeoWebMapping Enterprise">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1283372058576" ID="Freemind_Link_17300697" MODIFIED="1283372196955" TEXT="Objetivo: Fornecer uma interface de mapas na web com diversas funcionalidades de um gis web. Este produto dever&#xe1; possuir uma interface de administra&#xe7;&#xe3;o"/>
+<node CREATED="1283372210176" ID="Freemind_Link_1029666983" MODIFIED="1283372214727" TEXT="Modulos">
+<node CREATED="1283372215722" ID="Freemind_Link_970976578" MODIFIED="1283373168461" TEXT="GeoWebMapping Portal">
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1283372254500" ID="Freemind_Link_1536182403" MODIFIED="1283372322066" TEXT="Portal de mapas contextualizado de acordo com o perfil de acesso"/>
+<node CREATED="1283372328209" ID="Freemind_Link_652948614" MODIFIED="1283372334666" TEXT="Apresenta&#xe7;&#xe3;o de camadas de dados"/>
+<node CREATED="1283372335815" ID="Freemind_Link_419924135" MODIFIED="1283372345633" TEXT="Download de Informa&#xe7;&#xf5;es"/>
+<node CREATED="1283372346102" ID="Freemind_Link_1015353725" MODIFIED="1283372359817" TEXT="Cria&#xe7;&#xe3;o de filtros e consultas"/>
+<node CREATED="1283372476083" ID="Freemind_Link_1155956063" MODIFIED="1283372485298" TEXT="Inclus&#xe3;o de informa&#xe7;&#xf5;es geogr&#xe1;ficas"/>
+<node CREATED="1283372736345" ID="Freemind_Link_1173381137" MODIFIED="1283372738950" TEXT="Impress&#xe3;o de mapas"/>
+<node CREATED="1283373726913" ID="Freemind_Link_1207064018" MODIFIED="1283373732318" TEXT="Apontamento de erros nos mapas"/>
+<node CREATED="1283373732761" ID="Freemind_Link_1703427421" MODIFIED="1283373859840" TEXT="Cria&#xe7;&#xe3;o de formul&#xe1;rios com mapas"/>
+</node>
+<node CREATED="1283372223049" ID="Freemind_Link_448455510" MODIFIED="1283373170902" TEXT="GeoWebMapping Management">
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1283372493902" ID="Freemind_Link_688683355" MODIFIED="1283372588948" TEXT="Gerenciamento de perfis"/>
+<node CREATED="1283372508636" ID="Freemind_Link_1761883992" MODIFIED="1283372593651" TEXT="Gerenciamento de camadas"/>
+<node CREATED="1283372519819" ID="Freemind_Link_339694882" MODIFIED="1283372553824" TEXT="Publica&#xe7;&#xe3;o de mapas e funcionalidade no portal"/>
+<node CREATED="1283372560524" ID="Freemind_Link_1686589724" MODIFIED="1283372580979" TEXT="Gerenciamento de Servi&#xe7;os OGC(WMS,WFS)"/>
+</node>
+<node CREATED="1283372611255" ID="Freemind_Link_42496893" MODIFIED="1283373173245" TEXT="GeoWebMapping Styler">
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1283372619801" ID="Freemind_Link_1443497198" MODIFIED="1283372627553" TEXT="Defini&#xe7;&#xe3;o de estilos SLD para as camadas"/>
+</node>
+<node CREATED="1283372922425" ID="Freemind_Link_849887358" MODIFIED="1283373175707" TEXT="GeoWebMapping API">
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1283372929600" ID="Freemind_Link_1519828558" MODIFIED="1283372958794" TEXT="API JavaScript para desenvolvimento de aplica&#xe7;&#xf5;es webgis"/>
+<node CREATED="1283372960327" ID="Freemind_Link_183808516" MODIFIED="1283372967578" TEXT="Cria&#xe7;&#xe3;o de Widgets"/>
+<node CREATED="1283372968028" ID="Freemind_Link_1707694152" MODIFIED="1283372981667" TEXT="Protocolo de comunica&#xe7;&#xe3;o com o GeoWebMapping Portal"/>
+</node>
+</node>
+</node>
+<node CREATED="1283372648223" FOLDED="true" HGAP="21" ID="Freemind_Link_17574604" MODIFIED="1283373543004" TEXT="GeoInfrastructure" VSHIFT="6">
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1283372654665" HGAP="21" ID="Freemind_Link_755659371" MODIFIED="1283372690180" TEXT="Objetivo: Instanciar a stack de produtos para um cliente atrav&#xe9;s do conceito de cloud computer" VSHIFT="8"/>
+<node CREATED="1283373003349" ID="Freemind_Link_1965909988" MODIFIED="1283373005076" TEXT="Modulos">
+<node CREATED="1283373005871" FOLDED="true" ID="Freemind_Link_92848330" MODIFIED="1283373021527" TEXT="GeoInfrastructure Basic">
+<node CREATED="1283373023665" ID="Freemind_Link_117783980" MODIFIED="1283373058024" TEXT="Base Raster"/>
+<node CREATED="1283373037633" ID="Freemind_Link_1133239330" MODIFIED="1283373048094" TEXT="Base Vetorial (Postgis)"/>
+</node>
+</node>
+</node>
+<node CREATED="1283373545534" FOLDED="true" ID="Freemind_Link_679912227" MODIFIED="1283373554710" TEXT="GeoWorkProcess">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1283373556146" ID="Freemind_Link_1064150734" MODIFIED="1283373593494" TEXT="Objetivo: Definir um processo com v&#xe1;rios macro-processos ou disciplinas para se implantar um SDI baseado em toda a stack de produtos"/>
+<node CREATED="1283373676387" ID="Freemind_Link_1497034913" MODIFIED="1283373679391" TEXT="MacroProcessos">
+<node CREATED="1283373680190" ID="Freemind_Link_1495246245" MODIFIED="1283373685172" TEXT="Infrastrucuture"/>
+<node CREATED="1283373685625" ID="Freemind_Link_349928541" MODIFIED="1283373692740" TEXT="Profile/Security"/>
+<node CREATED="1283373693079" ID="Freemind_Link_1055552364" MODIFIED="1283373699486" TEXT="CatalogManagement"/>
+<node CREATED="1283373699772" ID="Freemind_Link_1203810444" MODIFIED="1283373707659" TEXT="MetaCatalogManagement"/>
+<node CREATED="1283373708515" ID="Freemind_Link_119686767" MODIFIED="1283373716264" TEXT="PortalManagement"/>
+</node>
+</node>
+<node CREATED="1283374103018" FOLDED="true" ID="Freemind_Link_712973567" MODIFIED="1283374167675" TEXT="GeoWorkStation" VSHIFT="5">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1283374110896" ID="Freemind_Link_1968602058" MODIFIED="1283374138860" TEXT="Objetivo: Distribui&#xe7;&#xe3;o Linux com softwares GIS instalados em conformidade com a stack GeoSpatial SDI"/>
+</node>
+<node CREATED="1283374181621" FOLDED="true" ID="Freemind_Link_1207065829" MODIFIED="1283374185170" TEXT="GeoTutorials">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1283374186245" ID="Freemind_Link_1296369944" MODIFIED="1283374206269" TEXT="Objetivo: Montar um conjunto de capacita&#xe7;&#xf5;es para atender toda a stack de produtos"/>
+</node>
+</node>
 </node>
 </map>
