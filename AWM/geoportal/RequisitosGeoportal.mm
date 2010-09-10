df69613763e715adb402c69ba09a43468bbf317c
Rafael Almeida Fernandez Soto <01057075540@serpro-1452916.bsa.serpro>
2010-09-10 10:51:08 -0300

    Criacao dos requisitos para o geoportal

---
+++ AWM/geoportal/RequisitosGeoportal.mm
@@ -0,0 +1,38 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1283455808387" ID="Freemind_Link_1900843590" MODIFIED="1283456042585" TEXT="GeoPortal">
+<node CREATED="1283456043631" ID="_" MODIFIED="1283456050049" POSITION="right" TEXT="Requisitos">
+<node CREATED="1283456242510" ID="Freemind_Link_1327024069" MODIFIED="1283457314662" TEXT="Menu de Mapas">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1283456257373" ID="Freemind_Link_1561716672" MODIFIED="1283456285161" TEXT="Tela inicial onde deve estar presente todos os mapas que est&#xe3;o dispon&#xed;veis para o perfil do usu&#xe1;rio logado"/>
+</node>
+<node CREATED="1283456292883" ID="Freemind_Link_228238721" MODIFIED="1283956993943" TEXT="Tela do Mapa" VSHIFT="11">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1283456336443" ID="Freemind_Link_1102601567" MODIFIED="1283456349667" TEXT="Tela central onde devem estar presentes todos os recursos configurados para um dado mapa"/>
+<node CREATED="1283456445353" ID="Freemind_Link_1105008126" MODIFIED="1283457314660" TEXT="Componentes">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1283456451154" ID="Freemind_Link_1176104488" MODIFIED="1283457314659" TEXT="Arvore de Temas">
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1283456551458" ID="Freemind_Link_639729460" MODIFIED="1283456670457" TEXT="Listagem de todas as camadas habilitadas de forma hierarquica"/>
+<node CREATED="1283456817255" ID="Freemind_Link_1165930613" MODIFIED="1283458396167" TEXT="Remover uma camada"/>
+<node CREATED="1283457763112" ID="Freemind_Link_1300139515" MODIFIED="1283458390560" TEXT="Deslocar a camada para cima e para baixo da hierarquia"/>
+<node CREATED="1283457782503" ID="Freemind_Link_871625492" MODIFIED="1283458383447" TEXT="Definir opacidade da camada"/>
+<node CREATED="1283458161208" ID="Freemind_Link_1664042770" MODIFIED="1283458379192" TEXT="Apresenta&#xe7;&#xe3;o da legenda de uma camada"/>
+<node CREATED="1283458267342" ID="Freemind_Link_1153112090" MODIFIED="1283458368333" TEXT="Enquadramento dentro dos universo de fei&#xe7;&#xf5;es da camada"/>
+<node CREATED="1283459156281" ID="Freemind_Link_218794416" MODIFIED="1283459166211" TEXT="Marcar e desmarcar todas as camadas para apresenta&#xe7;&#xe3;o"/>
+<node CREATED="1283458289914" ID="Freemind_Link_1003419345" MODIFIED="1283459135453" TEXT="Listagem de todos os dados das fei&#xe7;&#xf5;es em uma tabela paginada">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1283458336463" ID="Freemind_Link_1356862219" MODIFIED="1283458356780" TEXT="Sele&#xe7;&#xe3;o de um registro e enquadramento deste registro no mapa"/>
+<node CREATED="1283459020278" ID="Freemind_Link_1174406288" MODIFIED="1283459033291" TEXT="Op&#xe7;&#xe3;o de impress&#xe3;o como relat&#xf3;rio"/>
+<node CREATED="1283459034388" ID="Freemind_Link_197611313" MODIFIED="1283459048332" TEXT="Op&#xe7;&#xe3;o de remo&#xe7;&#xe3;o de colunas e registros"/>
+<node CREATED="1283459048771" ID="Freemind_Link_134098216" MODIFIED="1283459055878" TEXT="Op&#xe7;&#xe3;o de pagina&#xe7;&#xe3;o"/>
+</node>
+</node>
+<node CREATED="1283459175110" ID="Freemind_Link_870937728" MODIFIED="1283459183514" TEXT="Mapa">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
